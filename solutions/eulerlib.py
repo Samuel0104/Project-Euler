2f195670554e56f120a8664f5d4d90eec5173352
Samuel Moreno Vahos <95778014+Samuel0104@users.noreply.github.com>
2022-09-16 17:41:51 -0500

    Update eulerlib.py

--- solutions/eulerlib.py
+++ solutions/eulerlib.py
@@ -28,10 +28,10 @@ def isPrime(n: int) -> bool:
     Tests if n is a
     prime number
     """
-    if n < 2:
-        return False
     if n == 2:
         return True
+    if n < 2 or n%2 == 0:
+        return False
     for i in range(3, int(n**0.5) + 1, 2):
         if n%i == 0:
             return False
