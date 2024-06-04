56173a265da2838a0b442ee4ae850c663f3332a5
Samuel Moreno Vahos <95778014+Samuel0104@users.noreply.github.com>
2024-06-03 22:25:28 -0500

    Update p009.py

--- solutions/001 - 020/009/p009.py
+++ solutions/001 - 020/009/p009.py
@@ -1,12 +1,13 @@
 from eulerlib import time
 initial = time()
 
-for a in range(1, 333): # If a < b < c, the maximum possible value for a is 332
-    for b in range(a + 1, 500): # Same goes for b with the number 499
-        c = ((a**2) + (b**2))**0.5
-        if a + b + c == 1000:
-            print(int(a*b*c))
-            break
+def prob009():
+    for a in range(1, 333): # If a < b < c, the maximum possible value for a is 332
+        for b in range(a + 1, 500): # Same goes for b with the number 499
+            c = ((a**2) + (b**2))**0.5
+            if a + b + c == 1000:
+                return int(a*b*c)
+print(prob009())
 
 final = time()
 print(f"\nTime: {final-initial}")
