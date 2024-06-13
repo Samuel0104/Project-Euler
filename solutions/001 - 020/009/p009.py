48ad21985638741537bfc4f88a88fb47cb131bf4
Samuel Moreno Vahos <95778014+Samuel0104@users.noreply.github.com>
2024-06-13 09:35:38 -0500

    Update p009.py

--- solutions/001 - 020/009/p009.py
+++ solutions/001 - 020/009/p009.py
@@ -4,7 +4,7 @@ initial = time()
 def prob009():
     for a in range(1, 333): # If a < b < c, the maximum possible value for a is 332
         for b in range(a + 1, 500): # Same goes for b with the number 499
-            c = ((a**2) + (b**2))**0.5
+            c = (a**2 + b**2)**0.5
             if a + b + c == 1000:
                 return int(a*b*c)
 print(prob009())
